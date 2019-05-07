15926766bb7c6f22be8b1b726c3ea26bf961e17f
saiggo16 <santiagoiglesiasgonzalez@yahoo.es>
2019-05-07 22:36:31 +0200

    Main actaulizado

--- main.c
+++ main.c
@@ -6,32 +6,40 @@
 void *trabajo(void *tid)  {                                  // Código para cada thread
   int thid, i, j, k, ya, mis_recursos[Nmax];
   double x=0;thid= (int)tid;srand(thid+time(NULL));
-   for (i=0;i<Nmax;i++) {                                    // Adquiero nuevos recursos
+  for (i=0;i<Nmax;i++) {                                    // Adquiero nuevos recursos
      ya=0;
      while (ya==0) {                                          // Selecciono un recurso que no tengo
        j= (int) (Nmax)*(rand()/(RAND_MAX+1.0));
        ya=1;
-       for (k=0;k<i;k++) if (mis_recursos[k]==j) ya=0;
+       for (k=0;k<i;k++)
+         if (mis_recursos[k]==j) ya=0;
      }
      mis_recursos[i]=j;         // Incluyo recurso en mi lista
      printf("Soy %d y quiero el recurso %d\n",thid,j);
      pthread_mutex_lock(&recurso[j]);             // Adquiero el recurso
-     printf("           Soy %d y tengo el recurso %d\n",thid,j);
-     for (k=0;k<10000;k++) x+=sqrt(sqrt(k+0.1));      // Trabajo intrascendente
+     printf("      Soy %d y tengo el recurso %d\n",thid,j);
+
+     for (k=0;k<10000;k++) x+=sqrt(sqrt(k+0.1));    // Trabajo intrascendente
      k= (int) Tmin+(Tmax-Tmin+1)*(rand()/(RAND_MAX+1.0)) + 1;
      sleep(k);                                                     // Espero un tiempo aleatorio
    }
-   for (i=0;i<Nmax;i++)  pthread_mutex_unlock(&recurso[mis_recursos[i]]);
+   for (i=0;i<Nmax;i++)  
+     pthread_mutex_unlock(&recurso[mis_recursos[i]]);
      printf("************ACABE! Soy %d\n",thid);
    }
 
-   main() {
+void main() {
      int i, j;
      pthread_t th[Pmax];
      int tiempo_ini, tiempo_fin;       // Para medir tiempo
      for (i=0;i<Nmax;i++) pthread_mutex_destroy(&recurso[i]);
      for (i=0;i<Nmax;i++) pthread_mutex_init(&recurso[i], NULL);
+
      tiempo_ini=time(NULL);
+
      for (i=0;i<Pmax;i++) pthread_create(&th[i], NULL, trabajo, (void *)i);
      for (i=0;i<Pmax;i++) pthread_join(th[i], NULL);
-     tiempo_fin=time(NULL);printf(“Acabado en %d segundos\n”, tiempo_fin-tiempo_ini); for (i=0;i<Nmax;i++) pthread_mutex_destroy(&recurso[i]);           }
+     tiempo_fin=time(NULL);
+     printf("Acabado en %d segundos\n", tiempo_fin-tiempo_ini);
+     for (i=0;i<Nmax;i++) pthread_mutex_destroy(&recurso[i]);           
+}
