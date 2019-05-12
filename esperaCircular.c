9f89ea523fc81653db7ee4248e46a2e15adac94c
saiggo16 <santiagoiglesiasgonzalez@yahoo.es>
2019-05-12 20:26:33 +0200

    Espera circular iniciado

--- esperaCircular.c
+++ esperaCircular.c
@@ -8,27 +8,28 @@
 
 #define Tmin 0 // Tiempo mínimo de espera tras adquirir recurso
 #define Tmax 2 // Tiempo máximo de espera tras adquirir recurso
-#define Nmax 5 // Recursos
-#define Pmax 3 // Threads
+#define Nmax 2 // Recursos
+#define Pmax 2 // Threads
 
 pthread_mutex_t recurso[Nmax];
 
 void * trabajo(void * tid) { // Código para cada thread
-  int thid, i, j, k, ya, mis_recursos[Nmax];
+  int thid, i, j, k, ya, mis_recursos[Nmax],rec=0;
   double x = 0;
   thid = (intptr_t) tid;
   srand(thid + time(NULL));
   for (i = 0; i < Nmax; i++) { // Adquiero nuevos recursos
     ya = 0;
     while (ya == 0) { // Selecciono un recurso que no tengo
-      j = (int)(Nmax) * (rand() / (RAND_MAX + 1.0));
+      j=rec;
       ya = 1;
       for (k = 0; k < i; k++)
         if (mis_recursos[k] == j) ya = 0;
     }
-    mis_recursos[i] = j; // Incluyo recurso en mi lista
+    if (mis_recursos[i] < j) mis_recursos[i] = j; // Incluyo recurso en mi lista en caso de ser mayor que el que ya tenemos
+    rec++;
     printf("Soy %d y quiero el recurso %d\n", thid, j);
-    pthread_mutex_lock( & recurso[j]); // Adquiero el recurso
+    pthread_mutex_lock(& recurso[j]); // Adquiero el recurso
     printf(" Soy %d y tengo el recurso %d\n", thid, j);
     for (k = 0; k < 10000; k++) x += sqrt(sqrt(k + 0.1)); // Trabajo intrascendente
     k = (int) Tmin + (Tmax - Tmin + 1) * (rand() / (RAND_MAX + 1.0)) + 1;
