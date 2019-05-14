88ebae805273601db53078df382af6ec9be36260
Ana Otro Mac <anamartinezleborans@MacBook-Air-de-Ana.local>
2019-05-14 21:36:11 +0200

    Cambios a exclusionMutua.c

--- exclusionMutua.c
+++ exclusionMutua.c
@@ -24,7 +24,7 @@ void trabajo(void * tid) { // Código para cada thread
   thid = (intptr_t) tid;
   srand(thid + time(NULL));
   Ri = (int)(Nmax) * (rand() / (RAND_MAX + 1.0)); //Numero de recursos aleatorios <=N
-  int i, j, k, ya, mis_recursos[Nmax];
+  int i, j, k, ya, mis_recursos[Ri];
   double x = 0;
 
   for (i = 0; i < Ri; i++) { // Adquiero nuevos recursos
@@ -35,21 +35,29 @@ void trabajo(void * tid) { // Código para cada thread
       for (k = 0; k < i; k++)
         if (mis_recursos[k] == j) ya = 0;
     }
-    mis_recursos[i] = j; // Incluyo recurso en mi lista
+
     printf("Soy %d y quiero el recurso %d\n", thid, j);
+
     pthread_mutex_lock( & recurso[j]); // Adquiero el recurso
+    insertarCola(buffer[j],thid);
+    pthread_mutex_unlock( &recurso[mis_recursos[i]]);
+
+    mis_recursos[i] = j; // Incluyo recurso en mi lista
     printf(" Soy %d y tengo el recurso %d\n", thid, j);
     for (k = 0; k < 10000; k++) x += sqrt(sqrt(k + 0.1)); // Trabajo intrascendente
+
     k = (int) Tmin + (Tmax - Tmin + 1) * (rand() / (RAND_MAX + 1.0)) + 1;
     sleep(k); // Espero un tiempo aleatorio
+
+    asignados[thid]=-1; //Indico que he acabado con el recurso
   }
-  for (i = 0; i < Nmax; i++) pthread_mutex_unlock( & recurso[mis_recursos[i]]);
+  //for (i = 0; i < Nmax; i++) pthread_mutex_unlock( & recurso[mis_recursos[i]]);
   printf("************ACABE! Soy %d\n", thid);
   pthread_exit(NULL);
 }
 
 void *trabajoDemonio (void * tid){
-  //Cada demonio tiene que tener uns cola de solicitud
+  //Cada demonio tiene que tener una cola de solicitud
 
   int thid=tid;
   int solicitud;
@@ -71,7 +79,7 @@ void *trabajoDemonio (void * tid){
 
 int main() {
   int i;
-  pthread_t th[Pmax],thDemonio[Nmax];;
+  pthread_t th[Pmax],thDemonio[Nmax];
   int tiempo_ini, tiempo_fin; // Para medir tiempo
 
   //Inicializamos asignados
